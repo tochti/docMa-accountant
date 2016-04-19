32d326ba15cd42e8508d4204a1296747546b0466
Tim Tochtermann <tim.tochtermann@gmail.com>
2016-04-19 13:01:25 +0200

    testing and coding verify api

--- service/vouchers.go
+++ service/vouchers.go
@@ -1,24 +1,113 @@
-package accountant
+package accountantService
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"os"
+
+	"gopkg.in/gorp.v1"
 
 	"github.com/tochti/docMa-accountant/service/accountingTxsFileReader"
 	"github.com/tochti/docMa-handler/accountingData"
+	"github.com/tochti/docMa-handler/docs"
 )
 
-// Find alle Buchungen die keinen Belge-Datei haben
-func FindAccountingTxsWithoutVouchers(filePath string, voucherFilesDir string) ([]accountingData.AccountingData, error) {
+// Find alle Buchungen die keinen Belge-Datei haben s. auch Verify Funktion
+func FindAccountingTxsWithoutVouchers(db *gorp.DbMap, filePath string, voucherFilesDir string) ([]accountingData.AccountingData, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
 		return []accountingData.AccountingData{}, err
 	}
 
-	txFile, err := accountingTxsFileReader.NewReader(f)
+	txFile := accountingTxsFileReader.NewReader(f)
+
+	q, d, err := makeSQLQueryFindVouchers(txFile)
 	if err != nil {
 		return []accountingData.AccountingData{}, err
 	}
 
-	q, err := makeSQLQuery(txFile)
+	fmt.Println(q)
+	result := &[]struct{ Name string }{}
+	_, err = db.Select(result, q, d...)
+	if err != nil {
+		return []accountingData.AccountingData{}, err
+	}
 
+	fmt.Println(result)
+
+	return []accountingData.AccountingData{}, nil
+
+}
+
+func makeSQLQueryFindVouchers(txFile *accountingTxsFileReader.AccountingTxsFileReader) (string, []interface{}, error) {
+	vTable1WhereTpl := fmt.Sprintf("doc_numbers.number=?")
+	vTable2WhereTpl := fmt.Sprintf(`
+		account_data.period_from <= ?
+		AND
+		account_data.period_to >= ?
+		AND
+		(
+			 account_data.account_number = ?
+			 OR
+			 account_data.account_number = ?
+		 )
+	`)
+	where1 := bytes.NewBufferString("")
+	where2 := bytes.NewBufferString("")
+
+	queryData := []interface{}{}
+
+	// Erzeuge den ersten Eintrag da hier kein OR verwendet werden kann
+	a, err := txFile.Read()
+	if err != nil {
+		if err == io.EOF {
+			return "", []interface{}{}, nil
+		}
+		return "", []interface{}{}, err
+	}
+
+	where1.WriteString(vTable1WhereTpl)
+	queryData = append(queryData, a.DocNumberRange+a.DocNumber)
+
+	where2.WriteString(vTable2WhereTpl)
+	queryData = append(queryData, a.DateOfEntry, a.DateOfEntry, a.CreditAccount, a.DebitAccount)
+
+	// Erzeuge restlichen where-Abfragen anlegen
+	for {
+		a, err := txFile.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return "", []interface{}{}, err
+		}
+
+		where1.WriteString(fmt.Sprintf(" OR %v", vTable1WhereTpl))
+		queryData = append(queryData, a.DocNumberRange+a.DocNumber)
+
+		where2.WriteString(fmt.Sprintf(" OR(%v)\n", vTable2WhereTpl))
+		queryData = append(queryData, a.DateOfEntry, a.DateOfEntry, a.CreditAccount, a.DebitAccount)
+	}
+
+	vTable1 := fmt.Sprintf(`
+		SELECT doc_numbers.doc_id as id
+		FROM %v as doc_numbers
+		WHERE (%v) 
+	`, docs.DocNumbersTable, where1.String())
+
+	vTable2 := fmt.Sprintf(`
+		SELECT account_data.doc_id as id
+		FROM %v as account_data
+		WHERE %v
+	`, docs.DocAccountDataTable, where2.String())
+
+	q := fmt.Sprintf(`
+		SELECT docs.name 
+		FROM %v as docs, ((%v) UNION (%v)) as ids
+		WHERE docs.id = ids.id
+		GROUP BY docs.name
+	`, docs.DocsTable, vTable1, vTable2)
+
+	return q, queryData, nil
 }
--- service/vouchers_test.go
+++ service/vouchers_test.go
@@ -1,42 +1,52 @@
-package accountant
+package accountantService
 
 import (
-	"io"
+	"fmt"
 	"os"
+	"path"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/tochti/docMa-accountant/service/accountingTxsFileReader"
 	"github.com/tochti/docMa-handler/accountingData"
 	"github.com/tochti/docMa-handler/docs"
 	"github.com/tochti/gin-gum/gumspecs"
 	"gopkg.in/gorp.v1"
 )
 
+var (
+	TestData   = "./test_data"
+	TestDBName = "testing"
+)
+
 func Test_FindAccountingTxsWithoutVouchers(t *testing.T) {
+	txFile := path.Join(TestData, "export.csv")
 	db := initGorpConn(t)
-	createVouchersInDB(t, db)
+	createVouchersInDB(t, db, txFile)
 
 	// remove db entry
 
-	a, err := FindAccountingTxsWithoutVouchers(AccountingTxsFile)
+	a, err := FindAccountingTxsWithoutVouchers(db, path.Join(TestData, "export.csv"), TestData)
+	fmt.Println(a)
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	// 29.08.2013;01.09.2013;"B";"7";"Gewerbeanmeldung Stadt KA";26,00;4390;1210;0;"001";"";26,00;"EUR"
 	eAccTx := accountingData.AccountingData{
-		DocDate:          time.Date(2013, 8, 29, 0, 0, 0, 0, time.UTC),
-		DateOfEntry:      time.Date(2013, 9, 1, 0, 0, 0, 0, time.UTC),
+		DocDate:          time.Date(2013, 12, 1, 0, 0, 0, 0, time.UTC),
+		DateOfEntry:      time.Date(2013, 12, 2, 0, 0, 0, 0, time.UTC),
 		DocNumberRange:   "B",
-		DocNumber:        "6",
-		PostingText:      "Lastschrift Strato",
-		AmountPosted:     7.99,
-		DebitAccount:     71003,
-		CreditAccount:    1210,
+		DocNumber:        "7",
+		PostingText:      "Gewerbeanmeldung Stadt KA",
+		AmountPosted:     26.00,
+		DebitAccount:     4390,
+		CreditAccount:    1310,
 		TaxCode:          0,
-		CostUnit1:        "",
+		CostUnit1:        "001",
 		CostUnit2:        "",
-		AmountPostedEuro: 7.99,
+		AmountPostedEuro: 26.00,
 		Currency:         "EUR",
 	}
 
@@ -45,7 +55,7 @@ func Test_FindAccountingTxsWithoutVouchers(t *testing.T) {
 
 }
 
-func createVouchersInDB(t *testing.T, db *gorp.DbMap) {
+func createVouchersInDB(t *testing.T, db *gorp.DbMap, txFile string) {
 	docs.AddTables(db)
 
 	err := db.DropTablesIfExists()
@@ -57,49 +67,76 @@ func createVouchersInDB(t *testing.T, db *gorp.DbMap) {
 		t.Fatal(err)
 	}
 
-	f, err := os.Open(AccountingTxsFile)
+	f, err := os.Open(txFile)
 	if err != nil {
 		t.Fatal(err)
 	}
-	r := NewReader(f)
+	r := accountingTxsFileReader.NewReader(f)
 
-	for n := 0; ; n++ {
-		a, err := r.Read()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
+	// Erzeuge Belegeintrag desen Belegzeitraum zu einer Buchung passt.
+	a, err := r.Read()
 
-			t.Fatal(err)
-		}
+	doc := docs.Doc{
+		Name: "test1.pdf",
+	}
 
-		d := docs.DocAccountData{
-			PeriodFrom:    a.DocDate.Add(-24 * time.Hour),
-			PeriodTo:      a.DocDate.Add(24 * time.Hour),
-			AccountNumber: a.CreditAccount,
-		}
-		err = db.Insert(&d)
-		if err != nil {
-			t.Fatal(err)
-		}
+	err = db.Insert(&doc)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-		docNumber := ""
-		if a.DocNumber != "" {
-			docNumber = a.DocNumber
-		}
-		dn := docs.DocNumber{
-			DocID:  d.DocID,
-			Number: docNumber,
-		}
-		err = db.Insert(&dn)
-		if err != nil {
-			t.Fatal(err)
-		}
+	docAccountData := docs.DocAccountData{
+		DocID:         doc.ID,
+		PeriodFrom:    a.DocDate.Add(-24 * time.Hour),
+		PeriodTo:      a.DocDate.Add(24 * time.Hour),
+		AccountNumber: a.CreditAccount,
+	}
+
+	err = db.Insert(&docAccountData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Erzeuge einen Belegeintrag desen Belegnummer zu einer Buchung passt.
+	a, err = r.Read()
+
+	doc = docs.Doc{
+		Name: "test2.pdf",
+	}
+
+	err = db.Insert(&doc)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	docNumber := a.DocNumberRange + a.DocNumber
+	dn := docs.DocNumber{
+		DocID:  doc.ID,
+		Number: docNumber,
+	}
+	err = db.Insert(&dn)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
 
+func setenvTest() {
+	os.Clearenv()
+
+	os.Setenv("MYSQL_USER", "tochti")
+	os.Setenv("MYSQL_PASSWORD", "123")
+	os.Setenv("MYSQL_HOST", "127.0.0.1")
+	os.Setenv("MYSQL_PORT", "3306")
+	os.Setenv("MYSQL_DB_NAME", TestDBName)
+}
+
 func initGorpConn(t *testing.T) *gorp.DbMap {
+	setenvTest()
+	//gumspecs.AppName = "test"
 	mysql := gumspecs.ReadMySQL()
+	if mysql == nil {
+		t.Fatal("Error in MySQL config")
+	}
 
 	c, err := mysql.DB()
 	if err != nil {
